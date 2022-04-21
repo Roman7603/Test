f5ff2a1c0db4a3515de00388eb32d7adb269e909
McMelonTV <62901799+McMelonTV@users.noreply.github.com>
2022-04-21 09:55:48 +0200

    Update calc.cs

--- calc.cs
+++ calc.cs
@@ -7,17 +7,17 @@ namespace Test
     {
 
         private int a { get; set; }
-        private int b { get; set; }
+        private int c { get; set; }
 
-        public T1(int a, int b)
+        public T1(int a, int c)
         {
             this.a = a;
-            this.b = b;
+            this.c = c;
         }
 
         public int m1()
         {
-            return a + b;
+            return a + c;
         }
 
     }
